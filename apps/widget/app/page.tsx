924a63e0201870ec681d8b112aa31715c0428f25
mrlongruoi <mrlong01012020@gmail.com>
2025-08-17 13:36:12 +0700

    02: Convex Package

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,12 +1,19 @@
-import { Button } from "@workspace/ui/components/button"
+"use client";
+
+import { useMutation } from 'convex/react';
+import { api } from "@workspace/backend/_generated/api";
+import { Button } from '@workspace/ui/components/button';
+import { UserButton, OrganizationSwitcher } from "@clerk/nextjs";
 
 export default function Page() {
+  const addUser = useMutation(api.users.add);
+
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello apps/widget</h1>
-        <Button size="sm">Button</Button>
-      </div>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <p>Apps/Widget</p>
+      <UserButton />
+      <OrganizationSwitcher hidePersonal/>
+      <Button onClick={() => addUser()}>Add</Button>
     </div>
   )
 }
---
+++ packages/backend/convex/users.ts
@@ -0,0 +1,34 @@
+import { mutation, query } from "./_generated/server";
+
+export const getMany = query({
+  args: {},
+  handler: async (ctx) => {
+    const users = await ctx.db.query("users").collect();
+    return users;
+  },
+});
+
+export const add = mutation({
+  args: {},
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (identity === null) {
+      throw new Error("Không được xác thực");
+    }
+
+    const orgId = identity.orgId as string;
+
+    if (!orgId) {
+      throw new Error("Thiếu tổ chức");
+    }
+
+    // throw new Error("Kiểm tra theo dõi");
+
+    const userId = await ctx.db.insert("users", {
+      name: "Mrlongruoi",
+    });
+    
+    return userId;
+  },
+});
